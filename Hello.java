46f2d89e480ad18abbd5b35501676bd2c20df8e2
anto91github <antoz91@yahoo.com>
2024-05-11 15:06:12 +0700

    new update

--- Hello.java
+++ Hello.java
@@ -1,45 +1,33 @@
-class Hello 
+class A
+{
+    public A(){
+        super();
+        System.out.println(" this in A");
+    }
+
+    public A(int a){
+        super();
+        System.out.println(" this in A int " + a);
+    }
+}
+
+class B extends A
+{
+    public B(){
+        super();
+        System.out.println(" this is B");
+    }
+
+    public B(int b){
+        this(); // menjalankan constructor default pada class yg sama
+        System.out.println(" this is B int");
+    }
+}
+public class Hello 
 {
     public static void main(String a[])
     {
-        int num1 = 3;
-        int num2 = 2;
-        int result = num1 +num2;
-
-        byte by = 127;
-        short sh = 558;
-        long lg = 5854l;
-
-        float marks = 7.5f;
-        double marks2 = 7.5;
-
-        char k = 'k';
-        String sentence = "hello world";
-
-        boolean flag1 = true;
-        boolean flag2 = false;
-
-        //literals
-        int num3 = 10_000_000;
-
-        char c = 'a';
-        c++;
-
-        System.out.println(result);
-        System.out.println(by);
-        System.out.println(lg);
-        System.out.println("=================");
-        System.out.println(marks);
-        System.out.println(marks2);
-        System.out.println("=================");
-        System.out.println(k);
-        System.out.println(sentence);
-        System.out.println("=================");
-        System.out.println(flag1);
-        System.out.println(flag2);
-        System.out.println("=================");
-        System.out.println(num3);
-        System.out.println(c);
+        B obj = new B(5);
     }
 }
 
